bb4a80224cd39e2235d254205932d84ca7621b4b
adlandh <adlandh@gmail.com>
2017-10-27 20:21:21 +0300

    Init

--- bitrix_cleaner.go
+++ bitrix_cleaner.go
@@ -6,26 +6,27 @@ import (
 	"os"
 	"strings"
 	"runtime"
+	"path/filepath"
 )
 
 func main() {
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	path:=""
+	path:="."
 	all:=false
 	dirs:=[]string{"managed_cache","stack_cache","cache",`html_pages`}
 	done:=make(chan struct{},len(dirs))
 
-	flag.StringVar(&path, "path", "./", "Path to bitrix root")
-	flag.BoolVar(&all, "all", false,"All files or only expired")
+	flag.StringVar(&path, "path", ".", "Path to bitrix root")
+	flag.BoolVar(&all, "all", false,"all files (if not provided then only expired will be processed)")
 	flag.Parse()
 	path = strings.TrimSuffix(path, "/")
 
 	fileInfo, err := os.Stat(path + "/bitrix")
 	if err != nil {
 		if os.IsNotExist(err) {
-			if path == "." {
+			if path == "" {
 				path = "Current directory"
 			}
 			fmt.Println(path + " is not bitrix root. Use -h for help.")
@@ -41,8 +42,10 @@ func main() {
 	}
 
 	for _,dir := range dirs {
-		go processDir(path+"/bitrix/"+dir, done)
+		go processDir(path+"/bitrix/"+dir, all, done)
 	}
+
+	fmt.Println(all)
 
 	waitUntil(done,len(dirs))
 
@@ -55,7 +58,34 @@ func waitUntil(done <-chan struct{}, len int) {
 	}
 }
 
-func processDir(dir string, done chan<- struct{}) {
-	fmt.Println("Processing "+dir)
+func processDir(dir string, all bool, done chan<- struct{}) {
+	_,err:=os.Stat(dir)
+	if err != nil {
+		fmt.Println("Error processing "+dir)
+	} else {
+		if all {
+			os.RemoveAll(dir)
+			os.Mkdir(dir,0755)
+		} else {
+			filepath.Walk(dir,processExpiredFile)
+		}
+
+
+		fmt.Println("Done processing "+dir)
+	}
 	done <- struct{}{}
+}
+
+/* func processFile(path string, info os.FileInfo, err error) error {
+	fmt.Println(path)
+	fmt.Println(info.Size())
+	fmt.Println(info.IsDir())
+	return err
+} */
+
+func processExpiredFile(path string, info os.FileInfo, err error) error {
+	fmt.Println(path)
+	fmt.Println(info.Size())
+	fmt.Println(info.IsDir())
+	return err
 }
